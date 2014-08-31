cfdd25e580648a93784a01d54e3cd1fd0dc2a7a0
gtrivedi <trivedigaurav@gmail.com>
2014-08-30 21:27:07 -0400

    Added basic auth

---
+++ src/frontEnd/serverSide/AuthFilter.java
@@ -0,0 +1,57 @@
+package frontEnd.serverSide;
+
+import javax.ws.rs.WebApplicationException;
+import javax.xml.bind.DatatypeConverter;
+
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerRequestFilter;
+
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class AuthFilter implements ContainerRequestFilter {
+	@Override
+    public ContainerRequest filter(ContainerRequest containerRequest) throws WebApplicationException {
+
+//        String method = containerRequest.getMethod();
+//        String path = containerRequest.getPath(true);
+ 
+        String auth = containerRequest.getHeaderValue("authorization");
+        if(auth == null){
+            throw new WebApplicationException(Status.UNAUTHORIZED);
+        }
+ 
+        String[] credentials = decode(auth);
+ 
+        if(credentials == null || credentials.length != 2){
+            throw new WebApplicationException(Status.UNAUTHORIZED);
+        }
+ 
+        String uid = UserAuthentication.authenticate(credentials[0], credentials[1]);
+ 
+        if(uid == null){
+            throw new WebApplicationException(Status.UNAUTHORIZED);
+        }
+ 
+        return containerRequest;
+    }
+    
+    public static String[] decode(String auth) {
+        //Replacing "Basic THE_BASE_64" to "THE_BASE_64" directly
+        auth = auth.replaceFirst("[B|b]asic ", "");
+ 
+        //Decode the Base64 into byte[]
+        byte[] decodedBytes = DatatypeConverter.parseBase64Binary(auth);
+ 
+        //If the decode fails in any case
+        if(decodedBytes == null || decodedBytes.length == 0){
+            return null;
+        }
+ 
+        //Now split the byte[] into an array :
+        //  - the first one is login,
+        //  - the second one password
+        return new String(decodedBytes).split(":", 2);
+    }
+}
---
+++ src/frontEnd/serverSide/UserAuthentication.java
@@ -0,0 +1,51 @@
+package frontEnd.serverSide;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class UserAuthentication {
+	
+	private static String convertByteToHex(byte[] byteData) {
+
+	    StringBuilder sb = new StringBuilder();
+	    for (int i = 0; i < byteData.length; i++) {
+	        sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+	    }
+
+	    return sb.toString();
+	}
+	
+	public static String getHash(final String inputString) {
+
+	    MessageDigest md;
+	    
+		try {
+			md = MessageDigest.getInstance("SHA");
+			md.update(inputString.getBytes());
+
+		    byte[] digest = md.digest();
+		    
+		    return convertByteToHex(digest);
+
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+    public static String authenticate(String user, String password) {
+    	String uid = null; 
+    	
+    	System.out.print("User: " + user + " | Password: " + password + "\n");
+    	System.out.print(getHash(password) + "\n");
+    	
+//    	SHA for "password" is 5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8
+    	
+    	if (user.equals("username") && getHash(password).equals("5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8"))
+    		uid = "1";
+        
+    	return uid;
+    }
+}
